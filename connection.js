506d6a2a7cad3f3ac7449cd8c023660daae42051
JensC <jenscollaertprive@hotmail.com>
2023-06-04 09:24:14 +0200

    fix connection db

--- connection.js
+++ connection.js
@@ -11,43 +11,92 @@ const connection = mysql.createConnection({
   database: "gcbots"
 });
 
+// promise wrapper to enable async await with MYSQL
 connection.query = util.promisify(connection.query).bind(connection);
 
 const getData = () => {
-  return connection.query('SELECT * FROM mcxb');
-};
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM mcxb', (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 
 const updateDate = (gamertag) => {
-  const date = new Date().toISOString().split('T')[0];
-  const sql = `UPDATE mcxb SET DATE = '${date}' WHERE NAME = '${gamertag}'`;
-  return connection.query(sql);
-};
+  return new Promise((resolve, reject) => {
+    var date = (new Date()).toISOString().split('T')[0];
+    var sql = "UPDATE mcxb SET DATE ='" + date + "' WHERE NAME='" + gamertag + "'"
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 
 const updatePremium = (gamertag) => {
-  const sql = `UPDATE mcxb SET PREMIUM = 1 WHERE NAME = '${gamertag}'`;
-  return connection.query(sql);
-};
+  return new Promise((resolve, reject) => {
+    var sql = "UPDATE mcxb SET PREMIUM ='" + 1 + "' WHERE NAME='" + gamertag + "'"
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 
 const updateAdmin = (gamertag) => {
-  const sql = `UPDATE mcxb SET ADMIN = 1 WHERE NAME = '${gamertag}'`;
-  return connection.query(sql);
-};
+  return new Promise((resolve, reject) => {
+    var sql = "UPDATE mcxb SET ADMIN ='" + 1 + "' WHERE NAME='" + gamertag + "'"
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 
 const updateBotName = (gamertag, input) => {
-  const sql = `UPDATE mcxb SET BOTNAME = '${input}' WHERE NAME = '${gamertag}'`;
-  return connection.query(sql);
-};
+  return new Promise((resolve, reject) => {
+    var sql = "UPDATE mcxb SET BOTNAME ='" + input + "' WHERE NAME='" + gamertag + "'"
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 
 const updateDiscordID = (gamertag, id) => {
-  const sql = `UPDATE mcxb SET DISCORDID = '${id}' WHERE NAME = '${gamertag}'`;
-  return connection.query(sql);
-};
+  return new Promise((resolve, reject) => {
+    var sql = "UPDATE mcxb SET DISCORDID ='" + id + "' WHERE NAME='" + gamertag + "'"
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 
 const profileLookUp = (gamertag) => {
-  const sql = `SELECT * FROM mcxb WHERE NAME = '${gamertag}'`;
-  return connection.query(sql);
-};
-
+  return new Promise((resolve, reject) => {
+    var sql = "SELECT * FROM mcxb WHERE NAME = '" + gamertag + "'"
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
 export {
   connection,
   getData,
